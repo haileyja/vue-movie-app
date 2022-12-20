d511c54346850353d8a20ad2acbda3772014ea21
haileyja <somdari0423@gmail.com>
2022-12-20 23:24:10 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        name: 'Hailey',
+        email: 'somdari0423@gmail.com',
+        blog:'https://blog.naver.com/toystoiy',
+        phone:'+82-10-1234-5678',
+        image:'https://heropy.blog/css/images/logo.png'
+
+    })
+}
